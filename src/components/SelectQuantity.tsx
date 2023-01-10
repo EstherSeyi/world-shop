906a53caecbe667821798a6bc2a2aa663700f417
Esther Ogundijo <sheyiogundijo@gmail.com>
2023-01-10 23:41:13 +0100

    updates select quantity component

--- src/components/SelectQuantity.tsx
+++ src/components/SelectQuantity.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import * as Select from "@radix-ui/react-select";
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+} from "@radix-ui/react-icons";
 
 import { useCart } from "../hooks/cart";
 
@@ -7,23 +12,24 @@ const SelectQuantity = ({ cartItem, handleDelete }: any) => {
   const [showTextInput, setShowTextInput] = useState(false);
   const [value, setValue] = useState(cartItem.quantity);
   const { dispatch } = useCart();
-  const handleSelectQty = (event: any) => {
-    if (Number(event.target.value) === 0) {
+  const handleSelectQty = (value: string) => {
+    if (Number(value) === 0) {
       return handleDelete(cartItem);
     }
-    if (Number(event.target.value) === 10) {
+    if (Number(value) === 10) {
       return setShowTextInput(true);
     }
     return dispatch({
       type: "CHANGE_QUANTITY",
       payload: {
         cartItem,
-        cartQuantity: Number(event.target.value),
+        cartQuantity: Number(value),
       },
     });
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
     setShowTextInput(false);
     if (Number(value) === 0) return handleDelete(cartItem);
     return dispatch({
@@ -54,26 +60,53 @@ const SelectQuantity = ({ cartItem, handleDelete }: any) => {
         </form>
       ) : (
         <div className="relative block">
-          <select
-            id="selectqty"
-            className="mr-2 focus:outline-none cursor-pointer focus:scale-105"
+          <Select.Root
             value={cartItem.quantity}
-            onChange={handleSelectQty}
+            onValueChange={handleSelectQty}
           >
-            {new Array(11).fill(0, 0, 12).map((_, index) => {
-              return (
-                <option className="text-xs" key={index} value={index}>
-                  {index === 0 ? "0 (Delete)" : index === 10 ? "10+" : index}
-                </option>
-              );
-            })}
-          </select>
-          <label
-            htmlFor="selectqty"
-            className="pl-2 pr-2.5 py-0.5 shadow text-sm rounded-md bg-[#f0f2f2] absolute left-[2px]"
-          >
-            Qty: {cartItem.quantity}
-          </label>
+            <Select.Trigger
+              className="flex items-center shadow text-sm rounded-md bg-[#f0f2f2] pl-2 pr-2.5 py-0.5"
+              aria-label="Item Quantity"
+            >
+              <Select.Value>
+                <span className="cursor-pointer">Qty: {cartItem.quantity}</span>
+              </Select.Value>
+              <Select.Icon className="w-4 flex items-center justify-center">
+                <ChevronDownIcon width={15} />
+              </Select.Icon>
+            </Select.Trigger>
+
+            <Select.Portal>
+              <Select.Content className="overflow-hidden bg-white rounded-md shadow">
+                <Select.ScrollUpButton className="SelectScrollButton">
+                  <ChevronUpIcon />
+                </Select.ScrollUpButton>
+                <Select.Viewport className="p-2 flex flex-col gap-2">
+                  {new Array(11).fill(0, 0, 12).map((_, index) => {
+                    return (
+                      <Select.Item
+                        className="text-xs cursor-pointer"
+                        key={index}
+                        value={index.toString()}
+                      >
+                        <Select.ItemText>
+                          {index === 0
+                            ? "0 (Delete)"
+                            : index === 10
+                            ? "10+"
+                            : index}
+                        </Select.ItemText>
+
+                        <Select.ItemIndicator className="absolute left-0 w-6 inline-flex justify-center items-center">
+                          <CheckIcon />
+                        </Select.ItemIndicator>
+                      </Select.Item>
+                    );
+                  })}
+                </Select.Viewport>
+              </Select.Content>
+            </Select.Portal>
+          </Select.Root>
         </div>
       )}
     </>
