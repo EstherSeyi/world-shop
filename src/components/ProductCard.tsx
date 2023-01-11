dd8c447d3166ac2a59aa0e02dad1eae891cb8d1f
Esther Ogundijo <sheyiogundijo@gmail.com>
2023-01-11 21:06:37 +0100

    update product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,33 +2,39 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { i18nCurrencyFormat } from "../helpers/format";
+import { getDenominationRange } from "../helpers/giftcard";
+import { useProductDetail } from "../hooks/product";
+import { GiftcardType } from "../types/giftcards";
 
-const ProductCard = ({ asset, handleAddToCart }: any) => {
+const ProductCard = ({
+  giftcard,
+}: // handleAddToCart,
+{
+  giftcard: GiftcardType;
+  // handleAddToCart: (giftcard: GiftcardType) => void;
+}) => {
+  const { setProduct } = useProductDetail();
+  const handleClick = () => {
+    setProduct(giftcard);
+    localStorage.setItem("currentProduct", JSON.stringify(giftcard));
+  };
   return (
-    <div className="rounded-md shadow">
-      <div className="relative w-full min-w-[200px] aspect-square mb-2 ">
-        <Image
-          src={asset.images[0]}
-          className="object-cover rounded-md"
-          fill
-          alt="product"
-        />
+    <Link href={`/products/${giftcard.productId}`} onClick={handleClick}>
+      <div className="rounded-md shadow">
+        <div className="relative w-full min-w-[200px] aspect-square mb-2 ">
+          <Image
+            src={giftcard.img}
+            className="object-cover rounded-md"
+            fill
+            alt={giftcard.name}
+          />
+        </div>
       </div>
-      <div className="px-4 pb-4">
-        <Link href="http://google.com">
-          <span className="font-medium">{asset.name}</span>
-        </Link>
-        <p className="font-medium text-xl mt-2">
-          {i18nCurrencyFormat(asset?.currency ?? "USD").format(asset.price)}
-        </p>
-        <button
-          onClick={() => handleAddToCart(asset)}
-          className="py-2 px-2 bg-yellow rounded-lg shadow text-sm mt-4 transition-all hover:font-bold focus:font-bold border-yellow focus:border-2"
-        >
-          Add to Cart
-        </button>
-      </div>
-    </div>
+      <p className="font-medium mt-2">{giftcard.name}</p>
+      <p className="font-light text-xl mt-2">
+        {getDenominationRange(giftcard)}
+      </p>
+    </Link>
   );
 };
 export default ProductCard;
